e9d7f9f64a34b0ddd45bf78a1430d906e6c323f8
Renan da Fonte <renandafonte@hotmail.com>
2018-04-15 18:35:46 -0300

    player walking

--- main.lua
+++ main.lua
@@ -1,11 +1,45 @@
 function love.load()
+	speed = 1
+	player = newplayer()
 end
 
 function love.keypressed(key)
+	if key == "right" then
+		player.walk(1)
+	end
+	if key == "left" then
+		player.walk(-1)
+	end
+	if key == "space" then
+		player.jump()
+	end
 end
 
-function love.update (dt)
+function love.update(dt)
 end
 
-function love.draw ()
+function love.draw()
+	player.draw()
+end
+
+function newplayer()
+	local x, y = 30, 200
+	local width, height = love.graphics.getDimensions()
+	return {
+		update = function(dt)
+		end,
+		draw = function()
+			love.graphics.setColor(0,255,0)
+			love.graphics.rectangle("line", x, y, 10, 30)
+			love.graphics.setColor(255,255,255)
+		end,
+		walk = function(dir)
+			x = x + (dir*speed)
+			if x > width then
+				x = 0
+			end
+		end,
+		jump = function()
+		end
+	}
 end
